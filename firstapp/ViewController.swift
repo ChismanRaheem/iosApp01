e93878bed62f1b18bec2284405ba6ba954270854
Raheem Chisman <raheem.chisman@microsoft.com>
2024-05-31 23:42:25 -0700

    Update

--- firstapp/ViewController.swift
+++ firstapp/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var Label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        imageView.image = UIImage(named: "riley2")
+    }
 }
 
